87c52ade6cf53599e159490f27468aad1ef3a480
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-02-01 18:37:32 -0800

    set up perceptron neural network for xor training

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-const synaptic = require('synaptic');
+// const synaptic = require('synaptic');
+const {Layer, Network} = require('synaptic');
+
+let inputLayer = new Layer(2);
+let hiddenLayer = new Layer(3);
+let outputLayer = new Layer(1);
+
+inputLayer.project(hiddenLayer, Layer.connectionType.ALL_TO_ALL);
+hiddenLayer.project(outputLayer, Layer.connectionType.ALL_TO_ALL);
+
+let neuralNetwork = new Network({
+  input: inputLayer,
+  hidden: [hiddenLayer],
+  output: outputLayer,
+});
+
+const learningRate = 0.3;
+for(let i = 0; i < 200000; i++){
+  neuralNetwork.activate([1,0]);
+  neuralNetwork.propagate(learningRate, [1]);
+
+  neuralNetwork.activate([0,1]);
+  neuralNetwork.propagate(learningRate, [1]);
+
+  neuralNetwork.activate([1,1]);
+  neuralNetwork.propagate(learningRate, [0]);
+
+  neuralNetwork.activate([0,0]);
+  neuralNetwork.propagate(learningRate, [0]);
+}
+
+neuralNetwork.activate([0,0]);
+neuralNetwork.activate([1,1]);
+neuralNetwork.activate([1,0]);
+neuralNetwork.activate([0,1]);
+
+console.log(neuralNetwork.activate([1,1]));
